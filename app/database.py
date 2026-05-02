0b422d7198c224f0ea589e05ee1e310d7bdd4cc1
Ozgur <ozgurdh13@gmail.com>
2026-05-02 23:03:41 +0300

    Updated

--- app/database.py
+++ app/database.py
@@ -1,15 +1,3 @@
-'''
-from sqlmodel import SQLModel, create_engine
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-engine = create_engine(sqlite_url, echo=True)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine) 
-'''
-
 from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "sqlite:///database.db"
